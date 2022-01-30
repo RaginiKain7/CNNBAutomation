d563066a8a988646c60457ed9b9635dd07f120dc
Ragini Kain <ragini@cnnbsolutions.com>
2022-01-30 18:00:34 +0530

    azure POC

--- EG_Kerastase_AutomationFramework/src/test/java/test/EGHomePageTest.java
+++ EG_Kerastase_AutomationFramework/src/test/java/test/EGHomePageTest.java
@@ -1,37 +1,50 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import pageObject.HomePage;
 
 public class EGHomePageTest {
 	
 	static WebDriver driver =null;
 	
-	@BeforeTest
-	public void setupTest() {
-		
+	@BeforeMethod
+	public void setup() {		
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
-	}
-	
-	@Test
-	public static void header()
-	{
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.kerastase.com.eg/en/");
-		Assert.assertTrue(pageObject.HomePage.headerLogo(driver).isDisplayed());
 		
 	}
 	
-	@AfterTest
-	public void tearDownTest()
+	@Test(priority = 1)
+	public void kerEGLogoTest()
+	{
+		boolean flag = false;
+		flag = pageObject.HomePage.headerLogo(driver).isDisplayed();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority = 2)
+	public void kerEGTitleTest()
+	{
+		System.out.println(driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "Kerastase Egypt | Hair Treatments for all Hair Types");	
+	}
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown()
 	{
 		driver.close();
 		driver.quit();
